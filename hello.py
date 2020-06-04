a67b7448b60197ff6c09aae07aa1c14bf035b5b2
Frank Hofmann <frank.hofmann@efho.de>
2020-06-04 17:11:13 +0200

    hello.py as a basic test script

---
+++ hello.py
@@ -0,0 +1,10 @@
+# -----------------------------------------------------
+# (C) 2020 Frank Hofmann <frank.hofmann@efho.de>
+#
+# hello-spack. A Hello, world! in Python in order 
+# to test the SPACK package manager (http://spack.io/)
+#
+# Published unter BSD License.
+# -----------------------------------------------------
+
+print("Hello, world!")
